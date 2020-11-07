9816637ae93ae799883c0c0c3953c0a820dcca1f
Nikolai Golub <nikolay.v.golub@gmail.com>
2020-11-07 16:30:52 +0100

    Completed

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -21,6 +20,17 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let vec: Vec<&str> = s.split(",").collect();
+        if vec.len() != 2 {
+            Err(Self::Err::new())
+        } else {
+            let name: String = vec[0].to_owned();
+            let age = vec[1].parse::<usize>();
+            match age {
+                Ok(age) => Ok(Person { name, age }),
+                Err(e) => Err(Self::Err::new())
+            }
+        }
     }
 }
 
--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -5,7 +5,6 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
 
 use std::sync::Arc;
 use std::thread;
@@ -15,13 +14,19 @@ struct JobStatus {
     jobs_completed: u32,
 }
 
+impl JobStatus {
+    pub fn inc_jobs(&mut self) {
+        self.jobs_completed += 1;
+    }
+}
+
 fn main() {
     let status = Arc::new(JobStatus { jobs_completed: 0 });
-    let status_shared = status.clone();
+    let mut status_shared = status.clone();
     thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
-            status_shared.jobs_completed += 1;
+            Arc::get_mut(&mut status_shared).unwrap().inc_jobs();
         }
     });
     while status.jobs_completed < 10 {
